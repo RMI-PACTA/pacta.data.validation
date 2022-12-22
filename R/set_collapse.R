b8ed26fb9be97d84e17062d0756e402dffdb39d0
CJ Yetman <cyetman@rmi.org>
2022-12-22 15:10:17 +0100

    improve `set_collapse()` (#10)

--- R/set_collapse.R
+++ R/set_collapse.R
@@ -1,4 +1,7 @@
 set_collapse <-
   function(x) {
-    paste0('"', paste0(unique(x), collapse = '","'), '"')
+    nas <- is.na(x)
+    has_na <- any(nas)
+    x <- sort(unique(x))
+    paste0(ifelse(has_na, "`<NA>`, ", ""), '"', paste0(x, collapse = '", "'), '"')
   }
