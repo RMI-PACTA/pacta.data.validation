b60ce2a7acf48c7c35ca782761393644406de65d
CJ Yetman <cyetman@rmi.org>
2022-12-31 15:09:09 +0100

    check that vector is character first

--- R/assert_valid_sectors_with_assets.R
+++ R/assert_valid_sectors_with_assets.R
@@ -1,5 +1,8 @@
 assert_valid_sectors_with_assets <-
   function(x, any.missing = FALSE, .var.name = checkmate::vname(x), add = NULL) {
+    if (!is.character(x)) {
+      return(checkmate::makeAssertion(x, "must be a character vector", .var.name, add))
+    }
     x <- sort(unique(unlist(strsplit(unique(x), " + ", fixed = TRUE))))
     assert_valid_sector(x, any.missing = any.missing, .var.name = .var.name, add = add)
   }
