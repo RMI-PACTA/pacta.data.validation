0d0dc424dfe9ba8fe8b8948ea9e5cf79cc4d4759
CJ Yetman <cyetman@rmi.org>
2023-04-04 18:08:43 +0200

    style and lint (#33)

--- R/set_collapse.R
+++ R/set_collapse.R
@@ -1,11 +1,15 @@
 set_collapse <-
   function(x) {
     na_msg <- NULL
-    if (any(is.na(x))) { na_msg <- "`<NA>`" }
+    if (any(is.na(x))) {
+      na_msg <- "`<NA>`"
+    }
 
     str_msg <- NULL
     x <- sort(unique(x), na.last = NA)
-    if (length(x) > 0) { str_msg <- paste0('"', x, '"') }
+    if (length(x) > 0) {
+      str_msg <- paste0('"', x, '"')
+    }
 
     paste0(c(na_msg, str_msg), collapse = ", ")
   }
